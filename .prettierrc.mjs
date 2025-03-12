a3eeda4c2d4c65c23965cbc6bdb74b77f8acfbd1
Joaquin Esteban <dev@joacoesteban.com>
2025-03-12 01:22:28 +0100

    FEAT, add prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Options}
+ */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+}
