307b31a08782c01549cc2ea48556fc7050034cdd
Joaquin Esteban <dev@joacoesteban.com>
2025-03-17 18:09:19 +0100

    FEAT, add react hook utilities

--- lib/react.ts
+++ lib/react.ts
@@ -1,5 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { ImageStyle, StyleProp, ViewStyle } from 'react-native'
+
+export type Style = StyleProp<ViewStyle>
+export type Constructor<T, K extends any[]> = new (...args: K) => T
 
 export function onMounted(fn: () => void) {
   useEffect(fn, [])
+}
+
+export function useInstance<T, K extends any[]>(
+  ctor: Constructor<T, K>,
+  ...args: K
+) {
+  return useOnce(() => new ctor(...args))
+}
+
+export function useOnce<T>(fn: () => T) {
+  const [value] = useState(fn)
+  return value
 }
