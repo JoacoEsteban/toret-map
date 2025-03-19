ffb4e3011deeb4743d735524833372019eb76e25
Joaquin Esteban <dev@joacoesteban.com>
2025-03-19 13:00:11 +0100

    FEAT, add react utilities

--- lib/react.ts
+++ lib/react.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
-import { ImageStyle, StyleProp, ViewStyle } from 'react-native'
+import { StyleProp, ViewStyle } from 'react-native'
+import { Observable, Subscription } from 'rxjs'
+import { bind } from '@react-rxjs/core'
 
 export type Style = StyleProp<ViewStyle>
 export type Constructor<T, K extends any[]> = new (...args: K) => T
@@ -18,4 +20,15 @@ export function useInstance<T, K extends any[]>(
 export function useOnce<T>(fn: () => T) {
   const [value] = useState(fn)
   return value
+}
+
+export function useSubscriptions(fn: () => Subscription[]) {
+  onMounted(() => {
+    const subs = fn()
+    return () => subs.forEach((sub) => sub?.unsubscribe())
+  })
+}
+
+export function bindHook<T>(obs: Observable<T>) {
+  return bind(obs)[0]
 }
