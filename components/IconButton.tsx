ddbb71a8ed91b79e633cccd1c44343bfd5564d1c
Joaquin Esteban <dev@joacoesteban.com>
2025-03-19 13:23:23 +0100

    FEAT, add support for size in IconButton

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,6 +1,6 @@
 import { MaterialIcons } from '@expo/vector-icons'
 import { ActivityIndicator, Pressable, StyleSheet } from 'react-native'
-import { ColorName, useThemeColor } from './Themed'
+import { ColorName, useTheme, useThemeColor, useThemedProp } from './Themed'
 import { useContext, useMemo } from 'react'
 import { GlobalContext } from '@/app/global-context'
 import { delayWhen, distinctUntilChanged, interval, of } from 'rxjs'
@@ -11,43 +11,50 @@ export const IconButton = ({
   onPress,
   icon,
   loading = false,
-  style = {},
+  style,
   backgroundColorName = 'background',
+  colorName = 'text',
+  size = 40,
+  iconSize,
 }: {
   onPress: () => void
   icon: React.ComponentProps<typeof MaterialIcons>['name']
   loading?: boolean
   style?: Style
   backgroundColorName?: ColorName
+  colorName?: ColorName
+  size?: number
+  iconSize?: number
 }) => {
   const backgroundColor = useThemeColor(backgroundColorName)
-  const styles = useMemo(
-    () =>
-      StyleSheet.create({
-        container: {
-          width: 40,
-          height: 40,
-          backgroundColor,
-          // backgroundColor: '#f0f',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: '100%',
-          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-        },
-      }),
-    [backgroundColor],
+  const color = useThemeColor(colorName)
+  const { shadow } = useTheme()
+  const safeIconSize = useMemo(
+    () => Math.min(size, iconSize ?? size * 0.7),
+    [size, iconSize],
   )
 
-  const size = 28
-  const color = useThemeColor('text')
+  const styles = useMemo(
+    () =>
+      ({
+        width: size,
+        height: size,
+        backgroundColor,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '100%',
+        boxShadow: shadow,
+      }) as const,
+    [backgroundColor, size, shadow],
+  )
 
   return (
-    <Pressable style={[styles.container, style]} onPress={onPress}>
+    <Pressable style={[styles, style]} onPress={onPress}>
       {loading ? (
-        <ActivityIndicator size={size} color={color} />
+        <ActivityIndicator size={safeIconSize} color={color} />
       ) : (
-        <MaterialIcons size={size} color={color} name={icon} />
+        <MaterialIcons size={safeIconSize} color={color} name={icon} />
       )}
     </Pressable>
   )
